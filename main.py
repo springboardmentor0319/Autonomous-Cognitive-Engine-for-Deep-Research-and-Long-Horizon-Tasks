30921fcdbd674bed9dd711f78a552ca0faf9bd47
sagarkanta228 <gsagarkanta@gmail.com>
2026-01-07 17:33:47 +0530

    Adding two new tools to the agent

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from datetime import datetime
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 from langchain.agents import create_agent
+from langchain.tools import tool
 
-#Model
+# Model
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0
 )
 
-#Tool
+# Web Search Tool
 tavily_search_tool = TavilySearch(
     max_results=3,
     topic="general"
 )
+@tool
+def web_search(query: str) -> str:
+    """web_search tool"""
+    result = tavily_search_tool.invoke({"query": query})
+    return str(result)
 
-#Create agent
+# Current Time Tool
+@tool
+def get_current_time() -> str:
+    """Returns the current system date and time."""
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Weather Tool
+@tool
+def get_weather(city: str, date: str) -> str:
+    """Returns weather information for a given city and date."""
+    return f"The weather in {city} on {date} is expected to be sunny."
+
+# Create agent
 agent = create_agent(
     model=model,
-    tools=[tavily_search_tool]
+    tools=[web_search, get_current_time, get_weather]
 )
 
-#User query
-query = "Explain autonomous cognitive engines with real-world examples"
+# User query
+query = "What is the current time? what will be the weather in Delhi tomorrow? Why do parrots talk?"
 
-#Run agent (streaming)
-for step in agent.stream(
-    {"messages": query},
-    stream_mode="values",
-):
-    step["messages"][-1].pretty_print()
+#Run agent
+response = agent.invoke({"messages": query})
+print(response)
